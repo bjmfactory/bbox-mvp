0c7743fe6ff8f44b6b097c04d812916ad7de87af
Ben Johnson <ben@bjmfactory.com>
2015-04-06 19:05:02 -0500

    adds session module skeleton

---
+++ sessions.js
@@ -0,0 +1,21 @@
+window.Sessions = {}
+
+// Model
+Sessions.signIn = function (username, password) {
+  // Stub success!
+  return m.deferred.resolve({ token: 'abc123' })
+  // Stub failure
+  // return m.deferred.reject({ error: 'invalid_credentials' })
+}
+
+// Controller
+Sessions.controller = function () {
+
+}
+
+// View
+Sessions.view = function(ctrl) {
+
+}
+
+
