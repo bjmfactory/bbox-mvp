ee2f941b31671ee8be9f2394ede55c3046bc9092
Ben Johnson <ben@bjmfactory.com>
2015-04-07 11:34:12 -0500

    adds form element to sessions, mocks out session.submit( )

--- components/sessions.js
+++ components/sessions.js
@@ -9,6 +9,10 @@ Sessions.model = function () {
 Sessions.signIn = function (username, password) {
   // Stub success!
   return m.deferred.resolve({ token: 'abc123' })
+  // return m.request({
+  //   url: '/signin', method: 'post',
+  //   data: { username: username, password: password }
+  // })
   // Stub failure
   // return m.deferred.reject({ error: 'invalid_credentials' })
 }
@@ -16,24 +20,38 @@ Sessions.signIn = function (username, password) {
 // Controller
 Sessions.controller = function () {
   var ctrl = this
-  ctrl.sessions = m.prop( [new Sessions.model()] )
+  ctrl.session = new Sessions.model()
+
+  ctrl.submit = function(){
+    Sessions.signIn().then(
+      function (response) {
+        response.token //=> abc123
+        // redirect to links
+
+      },
+      function () {
+        // log error message
+      }
+    )
+  }
 }
 
 // View
 Sessions.view = function(ctrl) {
   // return m('h2', 'hello world from sessions')
-  return m('.sessionForm' , [
+  return m('form.sessionForm', {onsubmit: ctrl.submit}, [
     m('h1', 'Enter your info'),
     m('p', 'Username:'),
     m('input[type=text]', {
-      value: '',
-      onchange: m.withAttr('value', ctrl.sessions.username)
+      value: ctrl.session.username(),
+      onchange: m.withAttr('value', ctrl.session.username)
     }),
     m('p', 'Password:'),
     m('input[type=text]', {
-      value: '',
-      onchange: m.withAttr('value', ctrl.sessions.password)
+      value: ctrl.session.password(),
+      onchange: m.withAttr('value', ctrl.session.password)
     }),
+    m('input[type=submit]', {value: 'Log in'})
   ])
 }
 
