690c1ab8c51ef1dc56c418bcbbaf6de2fb8f1f2e
Ben Johnson <ben@bjmfactory.com>
2015-04-06 19:59:53 -0500

    fills out more of sessions model, controller, view

--- components/sessions.js
+++ components/sessions.js
@@ -1,6 +1,11 @@
 window.Sessions = {}
 
 // Model
+Sessions.model = function () {
+  this.username = m.prop('')
+  this.password = m.prop('')
+}
+
 Sessions.signIn = function (username, password) {
   // Stub success!
   return m.deferred.resolve({ token: 'abc123' })
@@ -10,12 +15,22 @@ Sessions.signIn = function (username, password) {
 
 // Controller
 Sessions.controller = function () {
-
+  var ctrl = this
 }
 
 // View
 Sessions.view = function(ctrl) {
-  return m('h1', 'Hello World from Sessions')
+  return m('.sesssionsForm' , [
+    m('h1', 'Enter your info'),
+    m('input[type=text]', {
+      value: '',
+      onchange: m.withAttr('value', ctrl.session.username)
+    }),
+    m('input[type=text]', {
+      value: '',
+      onchange: m.withAttr('value', ctrl.session.password)
+    }),
+  ])
 }
 
 
